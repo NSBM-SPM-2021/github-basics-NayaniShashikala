8d312602773fc6549376ac7fbbdc7985c96036f1
NayaniShashikala <56906401+NayaniShashikala@users.noreply.github.com>
2021-08-24 18:17:36 +0530

    CSS file added to the home page

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import './index.css'
 import Header from "./components/Header/Header";
 import Footer from './components/Footer/Footer';
 import Home from "./pages/Home/Home";
+import Quiz from "./pages/Quiz/Quiz";
 
 function App() {
   return (
      <BrowserRouter>
       <div className="app" style = {{backgroundImage:'url(./public/Group 792.png)'}}>
         <Header />
-
         <Switch>
-
           <Route path = '/' exact>
             <Home />
+          </Route>
+          <Route path = '/quiz' exact>
+            <Quiz />
           </Route>
 
         </Switch>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,12 @@
+import "./Home.css";
 const Home = () => {
-    return <div>Home Page</div>
+    return <div className='content'>
+        <div className='settings'>
+            <span style={{fontSize:30 }}>Quiz Settings</span>
+        </div>
+        <img src ='/quiz2.svg' className= 'banner' alt="quiz img"></img>
+
+    </div>
     
 };
 

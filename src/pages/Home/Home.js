b50eb52dd3c1424b4a2c4359ecf5789df708cec6
NayaniShashikala <56906401+NayaniShashikala@users.noreply.github.com>
2021-08-26 09:44:38 +0530

    created input fields

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,28 @@
 import "./Home.css";
+import { TextField } from "@material-ui/core";
 const Home = () => {
-    return <div className='content'>
-        <div className='settings'>
-            <span style={{fontSize:30 }}>Quiz Settings</span>
-        </div>
-        <img src ='/quiz2.svg' className= 'banner' alt="quiz img"></img>
+    return( 
+        <div className='content'>
+            <div className='settings'>
+                <span style={{fontSize:30 }}>Quiz Settings</span>
 
-    </div>
-    
+             <div className='settings_select'>
+                    <TextField 
+                    style={{ marginBottom: 20}}
+                    label = 'Enter Your Name'
+                    variant="outlined"
+                    />
+                    <TextField
+                        select label="Select Category" 
+                        variant= "outlined"
+                        style= {{ marginBottom: 20 }} />                </div>
+             </div>
+            <img src ='/quiz2.svg' className= 'banner' alt="quiz img"></img>
+
+        </div>
+    );
+
+
 };
 
 export default Home;
